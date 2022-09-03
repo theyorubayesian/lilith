5ba2627a0916f8c0b23b4700d64bec044bec630d
Akintunde Oladipo <akin.o.oladipo@gmail.com>
2022-09-04 00:53:03 +0100

    update apiHost in environment

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a87f9d6ac58cf4839b7d7393858af52a-462658230.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
